cbedb445d62dc09ecf542f7bccfbdefc15ad8def
soyowp <soyowp@gmail.com>
2021-11-01 17:57:47 +0900

    프로미스,어싱크,어웨이트

--- promise.js
+++ promise.js
@@ -1,16 +1,33 @@
 //이제 콜백헬에서 탈출해보도록 하자
-let one = `첫번째`;
-let two = `두번째`;
-let three = `세번째`;
 
-const test = (str, timer) => {
-  setTimeout(() => {
-    console.log(str);
-  }, timer);
-};
+let process = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('비동기임!!!!!!!!!!!!');
+      resolve(); //resolve를 넣어줘야 동기 작동된다.
+    }, 1000);
+  });
+}
 
-// test(one, 1000);
-// test(two, 700);
-// test(three, 400);
+let process2 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('비동기2임!!!!!!!!!!!!');
+      resolve();
+    }, 500);
+  });
+}
 
-const process = (str, timer) => new Promise((resolve, reject) => {});
+let process3 = () => {
+  return new Promise((resolve, reject) => {
+    reject(new Error('에러야!'));
+  })
+}
+
+process()
+  .then(() => process3()).catch(() => console.log("에러가 나부러쓰!")) //에러 출몰시 .catch로 캐치 가능!
+  .then(() => process())
+  .then(() => process2())
+  .then(() => process())
+  .then(() => process2())
+  .then(() => process());
